dcafefa8e9267dc512b4ca710228ff8e4fbeb326
BensonGitau <bensonkamaugitau@gmail.com>
2024-06-06 13:30:00 +0300

    complete

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,37 @@
 class MySet:
+    def __init__(self, list=[]):
+        self.dictionary = {}
+        for value in list:
+            self.dictionary[value] = True
 
-    pass
+    def __repr__(self):
+        return f"Myset({set(self.dictionary.keys())})"
+
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self,value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        return self.dictionary.clear()
+    
+list1 = MySet([1,2,3,4,5])
+print(list1.add(6))
+print(list1.has(4))
+print(list1.delete(9))
+print(list1.size())
+print(list1.clear())
+
+
+    
+
+
